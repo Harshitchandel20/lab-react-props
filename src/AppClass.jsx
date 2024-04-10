27eae9aa250e370425aa9f15cba0321de647e650
Harshitchandel20 <harsitchandel100@.gmail.com>
2024-04-10 17:59:50 +0530

    done

---
+++ src/AppClass.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import "./App.css"
+
+
+export default class AppClass extends Component{
+
+  // code here
+  render(){
+    const imgg = this.props.imageData();
+  
+    return (
+  
+      <div className="App">
+        <div className="header">
+          <h1>Kalvium Gallary</h1>
+        </div>
+        <div className="image-grid">
+          {imgg.map((ele) => (
+            <img key={ele.id} src={ele.img} alt="" />
+          ))}
+        </div>
+      </div>
+  
+    )
+  }
+}
